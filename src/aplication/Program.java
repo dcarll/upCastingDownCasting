5a220f56fdbd54cf9d008fdadbe4235a43f78621
dcarll <diego_carlos110@hotmail.com>
2020-02-12 23:10:41 -0300

    Projeto criado

---
+++ src/aplication/Program.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import entities.BusinessAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BusinessAccount account = new BusinessAccount();
+
+	}
+
+}
